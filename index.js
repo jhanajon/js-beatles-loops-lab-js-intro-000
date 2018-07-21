5af066a5061e9197bb687e8aa75beed65a575a03
joe fisher <jhanajon@gmail.com>
2018-07-21 13:24:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ function johnLennonFacts(array) {
 return(fact)
 }
 
-function iLoveTheBeatles(array) {
-
+function iLoveTheBeatles(n) {
+  const n = i
+  var i = 0
+  var array = []
+  do {
+    array.push('I love the Beatles!')
+    i++
+  }
+ while (i <= 15);
+ return(array)
 }
