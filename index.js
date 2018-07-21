dc0f5bc5b17748163ab0215ba312757267a4f3ef
joe fisher <jhanajon@gmail.com>
2018-07-21 12:43:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+    }
+    return array
+  }
+
+function johnLennonFacts(array) {
+  var fact = []
+  var i = 0
+
+  while (i < array.length){
+  fact.push(`${array[i]}!!!`)
+  i++
+}
+return(fact)
+}
+
+// function iLoveTheBeatles(array) {
+//
+// }
